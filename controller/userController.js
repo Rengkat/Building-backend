14f3864bd58788b585accd53430061cb03e76c59
Rengkat <successinmotion11@gmail.com>
2024-02-11 06:36:44 +0100

    model added and login functionality

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,41 @@
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+// const bcyrpt = require('')
+const User = require("../model/userModel");
+const CustomError = require("../error/custom-error");
 const createUser = asyncHandler(async (req, res) => {
   const { firstName, surname, phone, email, password } = req.body;
-  res.status(201).json({ message: "account successfully created", success: true });
+  if (!firstName || !surname || !phone || !email || !password) {
+    throw new CustomError("Please enter fields", 400);
+  }
+  //check if user exiswt first
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    throw new CustomError("User already exist", 400);
+  } else {
+    const user = await User.create({
+      firstName,
+      surname,
+      phone,
+      email,
+      password,
+    });
+    res
+      .status(201)
+      .json({ message: "account successfully created", success: true });
+  }
 });
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  res.status(200).json({ message: "user successfully login", success: true });
+  if (!email || !password) {
+    throw new CustomError("Please enter all fields", 400);
+  }
+  const user = await User.findOne({ email });
+  const token = jwt.sign({user?._id, user?.firstName, user?.surname},process.env.JWT_SECRETE, {expiresIn:'2d'})
+  res.status(200).json({ message: "user successfully login", success: true,token });
 });
-const updateUser = asyncHandler(async (req, res) => {});
+const updateUser = asyncHandler(async (req, res) => {
+  const tokenHeaders = req.headers.authorization;
+
+});
 module.exports = { createUser, loginUser, updateUser };
--- model/userModel.js
+++ model/userModel.js
@@ -1,3 +1,9 @@
 const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({});
+const UserSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  surname: { type: String, required: true },
+  phone: { type: String, required: true, unique },
+  email: { type: String, required: true, unique },
+  password: { type: String, required: true },
+});
 module.exports = mongoose.model("User", UserSchema);
