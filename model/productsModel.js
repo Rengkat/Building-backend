a5851468c7857d3e324310c82496d3bc498ca119
Rengkat <successinmotion11@gmail.com>
2024-05-23 10:47:31 +0100

    product model

--- model/productsModel.js
+++ model/productsModel.js
@@ -1,2 +1,13 @@
 const mongoose = require("mongoose");
-const ProductsSchema = mongoose.Schema({});
+const ProductsSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  category: { type: Sting, required: true },
+  quantity: { type: Number, required: true },
+  image: { type: String, required: true },
+  reviews: [reviewsSchema],
+  rating: { type: Number, required: true, default: 0 },
+  brand: { type: String, required: true },
+});
+module.exports = mongoose.model("Products", ProductsSchema);
